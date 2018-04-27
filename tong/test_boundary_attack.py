dff1ca9d14a97f9573b3dc9d521437be43d7829f
Yuguang Tong <ygtong@berkeley.edu>
2018-04-27 00:31:30 -0700

    update test script

--- tong/test_boundary_attack.py
+++ tong/test_boundary_attack.py
@@ -6,6 +6,7 @@ import keras
 from keras.preprocessing import image
 from keras.applications.densenet import DenseNet121
 from keras.applications.densenet import decode_predictions
+from PIL import Image
 
 # Load pretrained DenseNet
 keras.backend.set_learning_phase(0)
@@ -55,7 +56,17 @@ adversarial = attack(cat_img,
                      verbose=True)
 global_iterations += iteration_size
 
-np.save('adversarial_image_{0}'.format(global_iterations), adversarial.image)
+def save_adv_image(adversarial, global_iterations):
+    save_image_name = 'adversarial_image_{0}_steps_{1}_calls.jpg'.format(
+        global_iterations, adversarial._total_prediction_calls)
+    adv_image = (adversarial.image + 1) * 255 / 2
+    adv_image = adv_image.astype(np.uint8)
+    adv_image_pil = Image.fromarray(adv_image)
+    adv_image_pil.save(save_image_name)
+    return
+
+save_adv_image(adversarial, global_iterations)
+# np.save('adversarial_image_{0}'.format(global_iterations), adversarial.image)
 
 for i in range(5):
     adversarial = attack(adversarial,
@@ -64,9 +75,11 @@ for i in range(5):
                          log_every_n_steps=10,
                          verbose=True)
     global_iterations += iteration_size
-    np.save('adversarial_image_{0}'.format(global_iterations), adversarial.image)
+    save_adv_image(adversarial, global_iterations)
+    # np.save('adversarial_image_{0}'.format(global_iterations), adversarial.image)
 
     # show results
     print(np.argmax(fmodel.predictions(adversarial.image)))
     preds = kmodel.predict(np.expand_dims(adversarial.image.copy(), 0))
     print("Top 5 predictions (adversarial: ", decode_predictions(preds, top=5))
+    print("make {0} model calls so far".format(adversarial._total_prediction_calls))
